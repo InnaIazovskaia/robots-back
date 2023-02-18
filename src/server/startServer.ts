679e5cc6e943588a915deb47d7acd785a7f37ebc
Inna Iazovskaia <inna.0961@mail.ru>
2023-02-18 19:34:58 +0100

    Create startServer and app in server/index.ts

---
+++ src/server/startServer.ts
@@ -0,0 +1,21 @@
+import type CustomError from "../CustomError/CustomError.js";
+import app from "./index.js";
+
+const startServer = async (port: number) =>
+  new Promise((resolve, reject) => {
+    const server = app.listen(port, () => {
+      resolve(server);
+    });
+
+    server.on("error", (error: CustomError) => {
+      let errorMessage = "Error on starting the server";
+
+      if (error.code === "EADRINUSE") {
+        errorMessage += ` The port ${port} is already in use`;
+      }
+
+      reject(new Error(errorMessage));
+    });
+  });
+
+export default startServer;
